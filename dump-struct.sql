4b98194176d120871a84bf47bc2fbfccf83564ee
Matheus Inocêncio <inolopesm@gmail.com>
2023-05-23 08:23:17 -0300

    exporta dump da estrutura do banco de dados

---
+++ dump-struct.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `empresa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'EM_ABERTO',
+  `endereco` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_login_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+);
